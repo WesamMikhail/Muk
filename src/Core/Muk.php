c1e548bdaefe208cad2d1ee83553727e1784f806
demonshalo <rotm_2009@hotmail.com>
2015-12-04 05:03:40 +0100

    Removed CLI output from CURL

--- src/Core/Muk.php
+++ src/Core/Muk.php
@@ -69,7 +69,6 @@ abstract class Muk {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_URL,$request->getUrl());
-        curl_setopt($ch, CURLOPT_VERBOSE, 1);
         curl_setopt($ch, CURLOPT_HEADER, 1);
 
         if($request->getMethod() == "POST"){
